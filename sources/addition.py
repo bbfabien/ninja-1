cca852c95e64beae854517ea3c452a185f359bf6
Hugo <hugopozzoli@gmail.com>
2016-07-05 16:12:50 +0200

    Back to origin addition.py exercise template

--- sources/addition.py
+++ sources/addition.py
@@ -1,9 +1,5 @@
-import math
-import pydash
-
 def addition(a, b):
     if isinstance(a, int) is not True or isinstance(b, int) is not True:
         return 'NaN'
 
-    return pydash.pluck([{'name': 'moe', 'age': 40}, {'name': 'larry', 'age': 50}], 'name')
-    #return int(a) + int(b)
+    return int(a) + int(b)
--- test.py
+++ test.py
@@ -13,11 +13,17 @@ class Test(unittest.TestCase):
         result = code.addition(a, b)
         self.assertEqual(result, 'NaN')
 
+    def test_b_number(self):
+        a = 2
+        b = 5
+        result = code.addition(a, b)
+        self.assertTrue(isinstance(result, int))
+
     def test_c_correctResult(self):
         a = 23
         b = 54
         result = code.addition(a, b)
-        self.assertEqual(result, ['moe', 'larry'])
+        self.assertEqual(result, 77)
 
 if __name__ == '__main__':
 	unittest.main()
